813f38b262eb98ae2f902cd62def405bd412b221
parksang-won <tkddnjs2575@naver.com>
2024-11-11 15:01:05 +0900

    refac: 카운트 label github dev branch push 변경

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'dev by counter',
+              'github dev branch push',
             ),
             Text(
               '$_counter',
