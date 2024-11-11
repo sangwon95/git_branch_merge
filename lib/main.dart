9186d9a685310a0f11f7ad24f3102b935fcd0fb4
parksang-won <tkddnjs2575@naver.com>
2024-11-11 15:37:52 +0900

    refac: 카운트 label도 원상태로 복구

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'github dev branch push',
+              'counter:',
             ),
             Text(
               '$_counter',
